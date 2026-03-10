220e442f163a5a63150c3d775ba25f4db9b84bf7
victorj19 <79459569+victorj19@users.noreply.github.com>
2026-03-09 19:36:05 -0500

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('<h1>¡Despliegue Exitoso!</h1><p>Pipeline DevSecOps funcionando correctamente.</p>');
+  res.send('<h1>¡Despliegue Exitoso! Pipeline DevSecOps funcionando correctamente.</h1><p>Victor Julian Caballero.</p>');
 });
 
 // Endpoint de salud para Kubernetes/ArgoCD
